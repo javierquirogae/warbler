59e5c3ec49f197046474f5a611f4590fd3a5bcc4
jav <javier.quiroga.e@gmail.com>
2023-03-18 16:10:47 -0400

    can edit user profile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, ProfileEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -175,11 +175,7 @@ def stop_following(follow_id):
     return redirect(f"/users/{g.user.id}/following")
 
 
-@app.route('/users/profile', methods=["GET", "POST"])
-def profile():
-    """Update profile for current user."""
 
-    # IMPLEMENT THIS
 
 
 @app.route('/users/delete', methods=["POST"])
@@ -386,3 +382,55 @@ def signup():
     do_login(user)
 
     return redirect("/")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/users/profile', methods=["GET"])
+def update_profile_Form():
+    """Update profile for current user."""
+    user = User.query.get_or_404(session[CURR_USER_KEY])
+    form = ProfileEditForm(obj=user)
+    return render_template('users/edit.html', form=form, user=user)
+    # IMPLEMENT THIS
+
+
+@app.route('/users/profile', methods=["POST"])
+def update_profile():
+    """Update profile for current user."""
+    user = User.query.get_or_404(session[CURR_USER_KEY])
+    form = ProfileEditForm(obj=user)
+    user.username = form.username.data
+    user.email = form.email.data
+    user.image_url = form.image_url.data
+    user.header_image_url = form.header_image_url.data
+    user.location = form.location.data
+    user.bio = form.bio.data
+    db.session.commit()
+    
+    return redirect(f"/users/{user.id}")
+
